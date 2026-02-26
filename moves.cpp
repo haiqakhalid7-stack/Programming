85a34d91fb5ca9a4dd369364430bfd04e5e3cdab
haiqakhalid7-stack <haiqakhalid7@gmail.com>
2026-02-26 23:06:33 +0500

    Add files via upload

---
+++ moves.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+main () {
+
+cout<<" Enter age: ";
+float age;
+cin >> age;
+
+cout<<" Enter moves: ";
+float moves;
+cin >> moves;
+
+float years;
+years= age/(moves+1);
+
+cout<<" Average years = "<<years;
+}  
+
+
