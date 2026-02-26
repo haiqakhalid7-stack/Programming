1a7df177314e6634c9b5854f65e7d899086bdb58
haiqakhalid7-stack <haiqakhalid7@gmail.com>
2026-02-26 20:59:37 +0500

    Add files via upload

---
+++ pointers.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+main () {
+
+cout<<" Enter 2 pointers: ";
+int pointersOne;
+cin >> pointersOne;
+
+cout<<" Enter 3 pointers: ";
+int pointersTwo;
+cin >> pointersTwo;
+
+int points;
+points= pointersOne * 2 + pointersTwo * 3;
+
+cout<<" Total points = "<<points;
+} 
