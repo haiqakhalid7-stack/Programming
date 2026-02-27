712fa7d63f606e26bb866b801236f3f43e31b70f
haiqakhalid7-stack <haiqakhalid7@gmail.com>
2026-02-27 12:52:11 +0500

    week 3 2 more tasks added

---
+++ Week3/Lab tasks/angles - Copy.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+main () {
+
+cout<<" Enter sides: ";
+int n;
+n>=3;
+cin >> n;
+
+int angles;
+angles= (n-2)*180;
+
+cout<<" Sum of angles = "<<angles;
+}
+
